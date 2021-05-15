ef5bf6babf862f4025bffbb2638668f2a77c30ce
Andrey Kropotin <tork713@gmail.com>
2021-05-15 08:41:11 +0700

    continue 15.05.21

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cats.apps.CatsConfig',
     'blog.apps.BlogConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
